728b278c6bc42c4c6afa562e4d546b9adf52e69e
av3k1995 <av3k1995@gmail.com>
2024-06-10 19:38:52 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = '7053287182:AAGgIkMHo0PflejKw6GkiZszxW52qYJ8MJs'
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+}
---
+++ extensions.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from config import keys
+
+class APIException(Exception):
+    pass
+
+class CryptoCoverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        if quote == base:
+            raise APIException(f'Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалосьобработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        print(json.loads(r.content))
+        total_base = json.loads(r.content)[keys[base]]
+
+        return total_base * amount
